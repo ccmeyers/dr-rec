73459eff9d089ab1a33587d252980c89218e5a0d
ccmeyers <ccmeyers@gmail.com>
2015-09-16 17:18:28 -0400

    adds doctor resource - data now persisting

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -3,22 +3,39 @@ var Header = require('./components/Header.jsx');
 var AddDoc = require('./components/AddDoc.jsx');
 var DoctorsList = require('./components/DoctorsList.jsx');
 
-var mockDrs = [
-  { id: 1, first_name: 'Bob', last_name: 'Smith', practice_name: 'Smith and Assoc', specialty: 'Optometry' },
-  { id: 2, first_name: 'Alice', last_name: 'Jones', practice_name: 'Jones and Assoc', specialty: 'OBGYN' },
-  { id: 3, first_name: 'Carol', last_name: 'White', practice_name: 'White and Assoc', specialty: 'Dentist' }
-];
-
 var Main = React.createClass({
   getInitialState: function(){
     return {
-      doctorsList: mockDrs
+      doctorsList: []
     }
   },
   addDoc: function(docToAdd){
-    var newList = this.state.doctorsList;
-    newList.unshift(docToAdd);
-    this.setState({ doctorsList: newList });
+    $.ajax({
+      type: 'POST',
+      url: '/doctors',
+      data: docToAdd,
+      success:function(savedDoc) {
+        var newList = this.state.doctorsList;
+        newList.unshift(savedDoc);
+        this.setState({ doctorsList: newList });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url:'/doctors',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({doctorsList: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   render: function(){
     return (
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,11 @@
+class DoctorsController < ApplicationController
+
+  def index
+    render json: Doctor.all
+  end
+
+  def create
+    doctor = Doctor.create(first_name: params[:first_name], last_name: params[:last_name], practice_name: params[:practice_name], specialty: params[:specialty])
+    render json: doctor
+  end
+end
